44d756a1b9119d8eaf31cb5c43a853533a45a8d7
suhwa-lee <tnghk9611@kumoh.ac.kr>
2023-07-11 21:39:33 +0900

    feat: 깃허브 로그인 이후 메인 페이지에서 유저 정보 받아오기
    로그인 되지 않은 상태에서 접근 시, 시작 페이지로 리다이렉트 처리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Start from 'pages/StartPage';
+import Main from 'pages/MainPage';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Start />} />
+          <Route path="/start" element={<Start />} />
+          <Route path="/" element={<Main />} />
         </Routes>
       </BrowserRouter>
     </div>
